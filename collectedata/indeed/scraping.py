34132aa57681d7d2232a16d73a07f4773c450989
Jean-François Pinçon <jpincon@siriuseurope.com>
2017-08-01 17:42:21 +0200

    réécriture du projet à partir de zéro

--- collectedata/indeed/scraping.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-
-
-#indeed_dataframe_Postulants(typeDePoste, codePostal, rayonPostulant, fraicheurCV)
--- test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import os
-
-
-courant = os.getcwd()
-t = os.path.dirname(os.path.dirname(courant))
-print(t)
